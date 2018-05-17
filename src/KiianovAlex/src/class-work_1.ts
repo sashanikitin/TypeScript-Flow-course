7d24ff4e16370865ab5549ca8873dfcdf613d07f
Alex <kiyanov.alex@gmail.com>
2018-05-18 00:45:01 +0300

    revertSentence in progress

--- src/KiianovAlex/src/class-work_1.ts
+++ src/KiianovAlex/src/class-work_1.ts
@@ -34,5 +34,43 @@ const summator = (...args: Array<number | string>): number => {
 console.log(summator(1, 2)); // 3
 console.log(summator("3", 4)); // 7
 console.log(summator(5, "6")); // 11
-console.log(summator()); // 11
 */
+
+const getUnique = (...args: Array<number | string>): Array<number | string> => {
+    const arr: Array<number | string> = [];
+    args.forEach((arg) => {
+        if (!arr.includes(arg)) {
+            arr.push(arg);
+        }
+    });
+    return arr;
+};
+/*
+console.log(getUnique(1, 2, 3, 4)); // [1, 2, 3, 4]
+console.log(getUnique(1, 2, 2, 3, 4)); // [1, 2, 3, 4]
+console.log(getUnique(3, 1, 2, 1, 2, 4)); // [3, 1, 2, 4]
+*/
+
+const revertSentence = (str: string): string => {
+    let result: string = "";
+    const words: string[] = str.split(" ");
+    console.log(words);
+    words.forEach((word) => {
+        let wordInProgress: string = "";
+        for (let i = 0; i < word.length; i++) {
+            if (/d/g.test(word[i])) {
+
+                wordInProgress = wordInProgress + word[i];
+           } else {
+                wordInProgress = word[i] + wordInProgress;
+           }
+        }
+        result = result + wordInProgress;
+    });
+
+    return result;
+};
+
+console.log(revertSentence("s1tar3t 2 hellow")); // t1rat3s 2 wolleh
+console.log(revertSentence("s1ta$%r3t 2 hel^low")); // t1ra$%t3s 2 wol^leh
+console.log(revertSentence("s1tar3t 2   low5")); // t1rat3s 2   wol5
